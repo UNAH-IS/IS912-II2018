212a65238a36e593a39f53c99c2a4a2aa2be5138
Erick Marín <erick.marin.v@gmail.com>
2018-06-06 09:33:07 -0600

    Etiquetas básicas

--- UnidadI/HTML/etiquetas-basicas/index.js
+++ UnidadI/HTML/etiquetas-basicas/index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var app = express();
 
 //Exponer una carpeta como publica, unicamente para archivos estaticos: .html, imagenes, .css, .js
-app.use(express.static("www"));
+app.use(express.static("public"));
 
 //Crear y levantar el servidor web.
 app.listen(3000);
