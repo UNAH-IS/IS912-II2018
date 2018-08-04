d9b73fb5b80edf1d6059409cc3494d7c146154e3
Erick Marín <erick.marin.v@gmail.com>
2018-08-04 15:57:11 -0600

    Ejercicio Whatsapp

--- UnidadII/Ejercicio1/whatsapp/index.js
+++ UnidadII/Ejercicio1/whatsapp/index.js
@@ -1,9 +1,30 @@
 var express  = require("express");
 var bodyParser = require("body-parser");
+var mysql = require("mysql");
 var app = express();
 
+var credenciales = {
+    user:"root",
+    password:"",
+    port:"3306",
+    host:"localhost",
+    database:"bd_whatsapp"
+};
 
 app.use(express.static("public")); //Middlewares
 app.use(bodyParser.json());
+
+app.get("/obtener-usuarios", function(request, response){
+    var conexion = mysql.createConnection(credenciales);
+    var sql = 'SELECT codigo_usuario, nombre_usuario, correo, url_imagen_perfil FROM tbl_usuarios';
+    var usuarios = [];
+    conexion.query(sql)
+    .on("result", function(resultado){
+        usuarios.push(resultado);
+    })
+    .on("end",function(){
+        response.send(usuarios);
+    });   
+})
 
 app.listen(3333);
--- UnidadII/Ejercicio1/whatsapp/public/js/controlador.js
+++ UnidadII/Ejercicio1/whatsapp/public/js/controlador.js
@@ -9,3 +9,39 @@ function seleccionarContacto(codigoContacto, nombreContacto){
 $("#btn-enviar").click(function(){
 	alert("Enviar mensaje: " + $("#txta-mensaje").val());
 });
+
+
+
+$(document).ready(function(){
+	//Esta funcion se ejecuta cuando la página esta lista
+	$.ajax({
+		url:"/obtener-usuarios",
+		dataType:"json",
+		success:function(respuesta){
+			console.log(respuesta);
+			for(var i=0; i<respuesta.length; i++){
+				$("#slc-usuario").append('<option value="'+respuesta[i].codigo_usuario+'">'+respuesta[i].nombre_usuario+'</option>');
+				$("#div-contactos").append(
+					`<div class="row sideBar-body" onclick="seleccionarContacto(${respuesta[i].codigo_usuario},'${respuesta[i].nombre_usuario}');">
+						<div class="col-sm-3 col-xs-3 sideBar-avatar">
+						<div class="avatar-icon">
+							<img src="${respuesta[i].url_imagen_perfil}">
+						</div>
+						</div>
+						<div class="col-sm-9 col-xs-9 sideBar-main">
+						<div class="row">
+							<div class="col-sm-8 col-xs-8 sideBar-name">
+							<span class="name-meta">${respuesta[i].nombre_usuario}</span>
+							</div>
+							<div class="col-sm-4 col-xs-4 pull-right sideBar-time">
+							<span class="time-meta pull-right">18:18
+							</span>
+							</div>
+						</div>
+						</div>
+					</div>`
+				);
+			}
+		}
+	});
+});
